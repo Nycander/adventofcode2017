d633092c76291312d58c5de5a5ed6759a5d3a3dc
Martin Nycander <martin.nycander@gmail.com>
2017-12-17 21:03:00 +0100

    Day 11

--- src/main/java/com/adventofcode/year2017/Day11.java
+++ src/main/java/com/adventofcode/year2017/Day11.java
@@ -4,56 +4,34 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day11 {
     public static void main(String... args) throws Exception {
         Path inputPath = Paths.get(Day11.class.getResource("/inputs/day11.txt").toURI());
 
-        Point point = new Point(0, 0);
+        CubePoint point = new CubePoint(0, 0, 0);
 
-        Files.lines(inputPath)
+        List<HexDirection> directions = Files.lines(inputPath)
                 .flatMap(s -> Stream.of(s.split(",")))
                 .map(HexDirection::parse)
-                .forEach(point::move);
+                .collect(Collectors.toList());
 
+        int furthestDistance = 0;
+        for (HexDirection direction : directions) {
+            point = point.apply(direction);
+            furthestDistance = Math.max(point.distanceToOrigo(), furthestDistance);
+        }
 
-        System.out.println(point);
+        System.out.println("Part 1:");
+        System.out.println(point.distanceToOrigo());
 
-        // Ok, so let's BFS to origo
-        Map<Point, Point> paths = new HashMap<>();
+        System.out.println("Part 2:");
+        System.out.println(furthestDistance);
 
-        Point start = new Point(0, 0);
-        Point end = point;
-        LinkedList<Point> todo = new LinkedList<>();
-        todo.add(start);
-        paths.put(start, start);
-
-        do {
-            Point current = todo.pollFirst();
-
-            Stream.of(HexDirection.values()).parallel()
-                    .forEach(dir -> {
-                        Point next = current.copyMoved(dir);
-                        if (paths.containsKey(next))
-                            return;
-
-                        paths.put(next, current);
-                        if (next.equals(end)) {
-                            System.out.println(constructPath(paths, next).size() - 1);
-                            System.exit(1);
-
-                        } else {
-                            todo.addLast(next);
-                        }
-
-                    });
-        } while (!todo.isEmpty());
     }
 
     private static List<Point> constructPath(Map<Point, Point> paths, Point end) {
@@ -71,123 +49,42 @@ public class Day11 {
         return path;
     }
 
-    private static class Point {
-        int x, y;
+    static class CubePoint {
+        private final int x, y, z;
 
-        public Point(int x, int y) {
+        CubePoint(int x, int y, int z) {
             this.x = x;
             this.y = y;
+            this.z = z;
         }
 
-        Point copyMoved(HexDirection dir) {
-            Point point = new Point(x, y);
-            point.move(dir);
-            return point;
+        CubePoint apply(HexDirection d) {
+            return new CubePoint(x + d.dx, y + d.dy, z + d.dz);
         }
 
-        void move(HexDirection hexDir) {
-            switch (hexDir) {
-                case NORTH:
-                    y -= 1;
-                    break;
-                case SOUTH:
-                    y += 1;
-                    break;
-                case NORTH_EAST:
-                case SOUTH_EAST:
-                    x += 1;
-                    break;
-                case NORTH_WEST:
-                case SOUTH_WEST:
-                    x -= 1;
-                    break;
-            }
-
-            if (x % 2 == 0) {
-                switch (hexDir) {
-                    case NORTH_EAST:
-                        y -= 1;
-                        break;
-                    case NORTH_WEST:
-                        y -= 1;
-                        break;
-                }
-            } else {
-                switch (hexDir) {
-                    case SOUTH_EAST:
-                        y += 1;
-                        break;
-                    case SOUTH_WEST:
-                        y += 1;
-                        break;
-                }
-            }
-        }
-
-        @Override
-        public String toString() {
-            return x + ";" + y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Point point = (Point) o;
-            return x == point.x &&
-                    y == point.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-
-        public boolean isOrigo() {
-            return x == 0 && y == 0;
-        }
-    }
-
-    private static class Action {
-        Point from;
-        Point to;
-
-        HexDirection dir;
-
-        public Action(Point from, Point to, HexDirection dir) {
-            this.from = from;
-            this.to = to;
-            this.dir = dir;
-        }
-
-        int length() {
-            return Math.abs(from.x - to.x) + Math.abs(from.y - to.y);
-        }
-
-        @Override
-        public String toString() {
-            return "Action{" +
-                    "from=" + from +
-                    ", to=" + to +
-                    ", dir=" + dir +
-                    '}';
+        int distanceToOrigo() {
+            return Math.max(Math.max(Math.abs(x), Math.abs(y)), Math.abs(z));
         }
     }
 }
 
 
 enum HexDirection {
-    NORTH("n"),
-    SOUTH("s"),
-    NORTH_EAST("ne"),
-    NORTH_WEST("nw"),
-    SOUTH_EAST("se"),
-    SOUTH_WEST("sw");
+    NORTH("n", 0, 1, -1),
+    SOUTH("s", 0, -1, 1),
+    NORTH_EAST("ne", 1, 0, -1),
+    NORTH_WEST("nw", -1, 1, 0),
+    SOUTH_EAST("se", 1, -1, 0),
+    SOUTH_WEST("sw", -1, 0, 1);
 
     private final String code;
+    final int dx, dy, dz;
 
-    HexDirection(String code) {
+    HexDirection(String code, int dx, int dy, int dz) {
         this.code = code;
+        this.dx = dx;
+        this.dy = dy;
+        this.dz = dz;
     }
 
     static HexDirection parse(String str) {
